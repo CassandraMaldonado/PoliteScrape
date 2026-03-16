fc58329981de8a696b0829c71e78f2c50e20eb36
Cassandra M. Sullivan <caseymr96@gmail.com>
2026-03-16 14:55:20 -0700

    Update hashing.py

--- code/utils/hashing.py
+++ code/utils/hashing.py
@@ -3,7 +3,6 @@
  import hashlib
  from typing import Union
 
-# Returns SHA-256 hex digest of string or bytes.
  def sha256_hex(data: Union[str, bytes]) -> str:
    if isinstance(data, str):
      data = data.encode("utf-8")
